60d7ca9d9e154e385d6fb9a921a4c9c044e35963
Pushkaltoocool <118830976+Pushkaltoocool@users.noreply.github.com>
2025-01-25 19:37:12 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for your message! I will get back to you soon.');
-});
